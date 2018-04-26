96e9cf58146a6bfdb9ab94e0780d1f84755a62c7
Catherine Salazar <catcreates13@gmail.com>
2018-04-26 01:58:56 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
 
   def kind
     #binding.pry
-    if (side_1 + side_2) <= side_3 || (side_2 + side_3) <= side_1 || (side_3 + side_1) <= side_2
+    if (side_1 + side_2) <= side_3 || (side_2 + side_3) <= side_1 || (side_3 + side_1) <= side_2 || (side_1 * side_2 * side_3 == 0)
 end
   end
 
