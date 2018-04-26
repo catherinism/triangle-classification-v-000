a615cdd6e0e90e09ce0a69437e61ed3b35410d74
Catherine Salazar <catcreates13@gmail.com>
2018-04-26 02:15:55 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,6 +18,9 @@ class Triangle
     elsif side_1 == side_2 && side_1 == side_3
       self.kind = :equilateral
 
+    elsif side_1 == side_2 && side_1 == side_3
+      self.kind = :equilateral
+
 end
   end
 
