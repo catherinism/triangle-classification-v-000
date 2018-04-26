3abeb58cf2e5442f8f67e3c1a39a71d407ac7cdb
Catherine Salazar <catcreates13@gmail.com>
2018-04-26 02:16:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,12 +16,12 @@ class Triangle
     if (side_1 + side_2) <= side_3 || (side_2 + side_3) <= side_1 || (side_3 + side_1) <= side_2 || (side_1 * side_2 * side_3 == 0)
 
       raise TriangleError
-      
+
     elsif side_1 == side_2 && side_1 == side_3
       self.kind = :equilateral
 
-    elsif side_1 == side_2 && side_1 == side_3
-      self.kind = :isosceles
+    #elsif side_1 == side_2 && side_1 == side_3
+    #  self.kind = :isosceles
 
 end
   end
