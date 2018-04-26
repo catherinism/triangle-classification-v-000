1acc9d12f6c01bbd1bca3fd3dc3dac7034782b19
Catherine Salazar <catcreates13@gmail.com>
2018-04-26 02:00:59 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,6 +15,8 @@ class Triangle
     #binding.pry
     if (side_1 + side_2) <= side_3 || (side_2 + side_3) <= side_1 || (side_3 + side_1) <= side_2 || (side_1 * side_2 * side_3 == 0)
       raise TriangleError
+    elsif equilateral == 
+      
 end
   end
 
