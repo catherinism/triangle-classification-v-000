79b4c6c412f11a6874eb2a257c6e33fe8ba91200
Catherine Salazar <catcreates13@gmail.com>
2018-04-26 02:29:07 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,11 +23,10 @@ class Triangle
     elsif side_1 == side_2 || side_1 == side_3 || side_2 == side_3
       self.kind = :isosceles
 
-    else
-      self.kind = :isosceles
-    end
-  end
+    else self.kind = :scalene
 
+end
+  end
 
   class TriangleError < StandardError
       def message
