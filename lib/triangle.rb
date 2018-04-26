4fa89911ee2a5015c4b19dba89991e6258d51507
Catherine Salazar <catcreates13@gmail.com>
2018-04-26 02:22:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,6 +23,9 @@ class Triangle
     elsif side_1 == side_2 || side_1 == side_3 || side_2 == side_3
       self.kind = :isosceles
 
+    elsif side_1 == side_2 || side_1 == side_3 || side_2 == side_3
+      self.kind = :isosceles
+
 end
   end
 
