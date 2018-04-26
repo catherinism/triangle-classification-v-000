ca23534785c522ec12f4141c3ae09205d1846d9f
Catherine Salazar <catcreates13@gmail.com>
2018-04-26 02:14:06 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Triangle
 
-  attr_accessor :side_1, :side_2, :side_3
+  attr_accessor :side_1, :side_2, :side_3, :kind
 
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
